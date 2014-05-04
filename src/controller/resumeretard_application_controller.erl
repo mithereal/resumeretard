ec15e066a2f9b4233ac83e7f7db2e87396c5f577
Jason <mithereal@gmail.com>
2014-05-03 22:55:06 -0700

    sync changes

--- src/controller/resumeretard_application_controller.erl
+++ src/controller/resumeretard_application_controller.erl
@@ -5,7 +5,7 @@ lost('GET', []) ->
     {ok, [{message,"unable to find page"}]}.
     
 version('GET', []) ->
-DATA = boss_db:find(settings, [{name, 'equals', "version"}]),
-RECORD= hd(DATA),
-VALUE=RECORD:value(),
-    {ok, [{version,VALUE}]}.
+%DATA = boss_db:find(settings, [{name, 'equals', "version"}]),
+%RECORD= hd(DATA),
+%VALUE=RECORD:value(),
+    {ok, [{version,appver}]}.
