d7935cd8a06b0aa0e15e72e914ad2af59d475944
Jason <mithereal@gmail.com>
2014-05-03 18:31:53 -0700

    refactored html

--- priv/static/js/geo.js
+++ priv/static/js/geo.js
@@ -1,10 +1,10 @@
 function success(position) {
   var mapcanvas = document.createElement('div');
-  mapcanvas.id = 'leftmapcontainer';
+  mapcanvas.id = 'mapcontainer';
   mapcanvas.style.height = '420px';
   mapcanvas.style.width = '420px';
   
-  document.querySelector('article').appendChild(mapcanvas);
+  document.querySelector('#jobsmap').appendChild(mapcanvas);
 
   var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   
@@ -17,7 +17,7 @@ function success(position) {
     },
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("leftmapcontainer"), options);
+  var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
 
   //var marker = new google.maps.Marker({
       //position: coords,
@@ -26,36 +26,8 @@ function success(position) {
   //});
 }
 
-function success2(position) {
-  var mapcanvas = document.createElement('div');
-  mapcanvas.id = 'rightmapcontainer';
-  mapcanvas.style.height = '420px';
-  mapcanvas.style.width = '420px';
-  
-  document.querySelector('article').appendChild(mapcanvas);
-
-  var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  
-  var options = {
-    zoom: 11,
-    center: coords,
-    mapTypeControl: false,
-    navigationControlOptions: {
-    	style: google.maps.NavigationControlStyle.SMALL
-    },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("rightmapcontainer"), options);
-
-  //var marker = new google.maps.Marker({
-      //position: coords,
-      //map: map,
-      //title:"You are here!"
-  //});
-}
 
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success2);
   navigator.geolocation.getCurrentPosition(success);
 } else {
   error('Geo Location is not supported');
