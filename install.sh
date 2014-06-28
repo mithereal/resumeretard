0ba36dfaf66bf4a0307f19768f24fb05bdf30020
Jason <mithereal@gmail.com>
2014-06-28 16:10:33 -0700

    switched deps

--- install.sh
+++ install.sh
@@ -1,27 +1,74 @@
 #!/bin/bash
 STARTDIR=$(pwd)
-VERSION=0.8.10
-echo "Enter the Chicago Boss Dir (if no dir is selected we will download chicagoboss into $HOME/ChicagoBoss-x.x.x)"
-read COMPILERDIR
+VERSION=0.8.12
+DESTINATION_HELP='-d Chicago Boss destination path'
+VERSION_HELP='-v sets the Chicago Boss Project version'
+OVERWRITE_HELP='-u will overwrite if ChicagoBoss exists and install in default dir ChicagoBoss (skips versioning, aka upgrade)'
 
-if [ "${COMPILERDIR}" > 3 ]
+function install()
+{
+  if [ -z "$COMPILERDIR" ]
 then
-cd ${COMPILERDIR}
-else
-## fetch and install chicagoboss
-cd ~
-wget https://github.com/ChicagoBoss/ChicagoBoss/archive/v"${VERSION}".tar.gz
-tar -xvzf v"${VERSION}".tar.gz
+  echo "Enter the Chicago Boss Dir (if no dir is selected we will download ChicagoBoss into $HOME/ChicagoBoss-${VERSION})"
+  read COMPILERDIR
 fi
 
+
+if [[ 1 > ${#COMPILERDIR} ]]
+then
+CBBDIR=${HOME}
+else
+CBBDIR=${COMPILERDIR}
+fi
+
+## fetch and install chicagoboss
+cd ${CBBDIR}
+wget https://github.com/ChicagoBoss/ChicagoBoss/archive/v"${VERSION}".tar.gz
+tar -xvzf v"${VERSION}".tar.gz
+
+if [ -z "$OVERWRITE" ]
+then
 cd ChicagoBoss-"${VERSION}"
+else
+rm -drf ChicagoBoss
+mv ChicagoBoss-"${VERSION}" ChicagoBoss
+cd ChicagoBoss
+fi
+
 COMPILERDIR=$(pwd)
+
 echo "...Building Chicago Boss..."
 make
 cd ${STARTDIR}
-
-sed s_/home/mithereal/ChicagoBoss.[[:digit:]]*.[[:digit:]]*.[[:digit:]]*_${COMPILERDIR}_ boss.config > boss.config
-sed s_/home/mithereal/ChicagoBoss.[[:digit:]]*.[[:digit:]]*.[[:digit:]]*_${COMPILERDIR}_ rebar.config > rebar.config
+#sed s_{path, \".*/ChicagoBoss.[[:digit:]]*.[[:digit:]]*.[[:digit:]]*_{path, \"${COMPILERDIR}_ rebar.config > rebar.config # may be needed in some cases
 ./rebar get-deps compile
+}
 
+function help()
+{
+    echo ""
+    echo "Resume Retard Installer by Jason Clark <mithereal@gmail.com>"
+    echo ""
+    echo "Usage: install $DESTINATION_HELP "
+    echo $VERSION_HELP
+}
 
+while getopts ":d:?:v" opt; do
+    case $opt in
+        d)
+            COMPILERDIR=$OPTARG
+            ;;
+        v)
+            VERSION=$OPTARG
+            ;;
+        u)
+            OVERWRITE=$OPTARG
+            ;;
+        ?)
+            help
+exit 0
+            ;;
+    esac
+done
+
+install
