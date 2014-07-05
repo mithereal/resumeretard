4baadee134b19bbc44e480b75133a9dd7c821650
Jason <mithereal@gmail.com>
2014-07-05 12:02:31 -0700

    create new job method

--- src/controller/resumeretard_job_controller.erl
+++ src/controller/resumeretard_job_controller.erl
@@ -3,19 +3,9 @@
 
 %% @doc Creates a new job record.
 create('POST', []) ->
-  Title = Req:param("title"),
-  Ownerid = Req:param("ownerid"),
-  Created = Req:param("created"),
-  Modified = Req:param("modified"),
-  Expires = Req:param("expires"),
-  Location = Req:param("location"),
-  Subtitle = Req:param("subtitle"),
-  Requirements = Req:param("requirements"),
-  Benifets = Req:param("benifets"),
-  Type = Req:param("type"),
-  Description = Req:param("description"),
+Attrs = [{title, Req:param("title")},{ownerid, Req:param("ownerid")},{created, Req:param("created")},{modified, Req:param("modified")},{expires, Req:param("expires")},{location, Req:param("location")},{subtitle, Req:param("subtitle")},{requirements, Req:param("requirements")},{benifets, Req:param("benifets")},{type, Req:param("type")},{description, Req:param("description")}],
 
-  Job = job:new(id,Title,Ownerid,Created,Modified,Expires,Location,Subtitle,Requirements,Benifets,Type,Description),
+  Job = boss_record:new('job', Attrs),
   Job:save(),
   
  {ok, [{job,Job}]}.
